2751bdb63ca0917f149cce2062e33acf42232289
preyPierian <preya@pierian.co>
2022-09-12 13:58:58 +0100

    struct & category object addition

---
+++ classification prgogram/category_obj.py
@@ -0,0 +1,13 @@
+class category_object : 
+    def __init__(self,str) :
+        self.drinkCat1 = ""
+        self.cat_1_pos = 9999
+        self.drinkCat2 = ""
+        self.cat_2_pos = 9999
+        self.drinkCat3 = ""
+        self.cat_3_pos = 9999
+        self.drinkCat4 = ""
+        self.cat_4_pos = 9999
+        self.flavoured =  0
+        self.rtd =  0
+        self.str = " "+str+" "
